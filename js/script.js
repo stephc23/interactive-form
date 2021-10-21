184e7246491f67ffe31923b39bfccb2eab7daa33
stephc23 <88057766+stephc23@users.noreply.github.com>
2021-10-20 20:22:49 -0400

    Hide/display other job text field

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // Set default focus state to name field
 document.querySelector('#name').focus();
+
+// Display 'Other job role' text field only when 'Other' is selected
+const jobRoleSelect = document.querySelector('#title');
+const otherJobInput = document.querySelector('#other-job-role');
+otherJobInput.style.display = 'none';
+jobRoleSelect.addEventListener('change', () => {
+    if (jobRoleSelect.value === 'other') {
+        otherJobInput.style.display = 'block';
+    } else {
+        otherJobInput.style.display = 'none';
+    }
+});
+
+
